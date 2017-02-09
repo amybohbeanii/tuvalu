a1780464a9a6acfca82302977637339968b1f62a
Samuel Bernheim <sambernheim@gmail.com>
2017-02-09 16:03:14 -0500

    Add gulp and gulp-scss for scss to css compilation

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('buildCSS', function () {
+	// The source scss file is a main file which just imports all the separate scss files
+	return gulp.src('./client/scss/index.scss')
+	.pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
+	.pipe(gulp.dest('./client/css/')) // write the css file to ./server
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./client/scss/*', ['buildCSS']);
+});
+
+gulp.task('default', ['buildCSS', 'watch']);
