0c705c0d8400ce670c66737a9694d214bbee68db
Samuel Bernheim <sambernheim@gmail.com>
2017-02-09 16:06:02 -0500

    Add css minification with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('buildCSS', function () {
 	// The source scss file is a main file which just imports all the separate scss files
 	return gulp.src('./client/scss/index.scss')
 	.pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
+	.pipe(cleanCSS())
 	.pipe(gulp.dest('./client/css/')) // write the css file to ./server
 });
 
